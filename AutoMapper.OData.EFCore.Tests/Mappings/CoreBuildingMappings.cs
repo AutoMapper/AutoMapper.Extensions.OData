2f402f3498bfc3b1dae3bab37fec829a0ca834ca
BlaiseD <btaylor@bPolicySolutions.com>
2020-04-18 06:31:21 -0400

    Removing IIncludableQueryable includes in favour of Expression<Func<TModel, object>> for ProjectTo in EFCore.

--- AutoMapper.OData.EFCore.Tests/Mappings/CoreBuildingMappings.cs
+++ AutoMapper.OData.EFCore.Tests/Mappings/CoreBuildingMappings.cs
@@ -10,11 +10,12 @@ namespace AutoMapper.OData.EFCore.Tests.Mappings
             CreateMap<TBuilding, CoreBuilding>()
                 .ForMember(d => d.Identity, o => o.MapFrom(s => s.Identity))
                 .ForMember(d => d.Name, o => o.MapFrom(s => s.LongName))
-                .ForMember(d => d.Builder, o => o.MapFrom(s => s.Builder))
-                .ForMember(d => d.Tenant, o => o.MapFrom(s => s.Mandator))
+                .ForMember(d => d.Builder, o => o.ExplicitExpansion())
+                .ForMember(d => d.Tenant, o => { o.MapFrom(s => s.Mandator); o.ExplicitExpansion(); })
                 .ForAllOtherMembers(o => o.Ignore());
 
-            CreateMap<TBuilder, OpsBuilder>();
+            CreateMap<TBuilder, OpsBuilder>()
+                .ForMember(d => d.City, o => o.ExplicitExpansion());
             CreateMap<TCity, OpsCity>();
         }
     }
