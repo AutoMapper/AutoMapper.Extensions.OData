6e9629fe57e834c86e171d4f90b618a41d164c7c
BlaiseD <btaylor@bPolicySolutions.com>
2020-08-20 10:42:07 -0400

    Testing Minver

--- Pack_Push.ps1
+++ Pack_Push.ps1
@@ -4,7 +4,7 @@ Write-Host "Owner ${Env:REPO_OWNER}"
 Write-Host "Repository ${Env:REPO}"
 
 $PROJECT_PATH = ".\$($Env:PROJECT_NAME)\$($Env:PROJECT_NAME).csproj"
-$NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).$($Env:VERSION_NUMBER).nupkg"
+$NUGET_PACKAGE_PATH = ".\artifacts\$($Env:PROJECT_NAME).*.nupkg"
 
 Write-Host "Project Path ${PROJECT_PATH}"
 Write-Host "Package Path ${NUGET_PACKAGE_PATH}"
@@ -13,5 +13,5 @@ if ($Env:REPO_OWNER -ne "AutoMapper") {
     Write-Host "${scriptName}: Only creates packages on AutoMapper repositories."
 } else {
     dotnet pack $PROJECT_PATH -c Release -o .\artifacts --no-build
-    dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate --source $Env:DEPLOY_PACKAGE_URL --api-key $Env:DEPLOY_PACKAGE_API_KEY
+#    dotnet nuget push $NUGET_PACKAGE_PATH --skip-duplicate --source $Env:DEPLOY_PACKAGE_URL --api-key $Env:DEPLOY_PACKAGE_API_KEY
 }
