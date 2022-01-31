3302b08c0739266a37b638522e06cc193779b8b0
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-01-31 09:13:26 -0500

    AutoMapperv11 and removing the obsolete.

--- AutoMapper.AspNetCore.OData.EF6/QueryableExtensions.cs
+++ AutoMapper.AspNetCore.OData.EF6/QueryableExtensions.cs
@@ -26,21 +26,6 @@ namespace AutoMapper.AspNet.OData
         /// <param name="query"></param>
         /// <param name="mapper"></param>
         /// <param name="options"></param>
-        /// <param name="handleNullPropagation"></param>
-        /// <returns></returns>
-        [Obsolete("\"Use ICollection<TModel> Get<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings)\"")]
-        public static ICollection<TModel> Get<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, HandleNullPropagationOption handleNullPropagation = HandleNullPropagationOption.Default)
-            where TModel : class
-            => query.Get(mapper, options, new QuerySettings { ODataSettings = new ODataSettings { HandleNullPropagation = handleNullPropagation } });
-
-        /// <summary>
-        /// Get
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="options"></param>
         /// <param name="querySettings"></param>
         /// <returns></returns>
         public static ICollection<TModel> Get<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings)
@@ -81,36 +66,6 @@ namespace AutoMapper.AspNet.OData
                 querySettings?.AsyncSettings
             );
         }
-
-        /// <summary>
-        /// GetAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="options"></param>
-        /// <param name="handleNullPropagation"></param>
-        /// <returns></returns>
-        [Obsolete("\"Use Task<ICollection<TModel>> GetAsync<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings = null)\"")]
-        public static async Task<ICollection<TModel>> GetAsync<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, HandleNullPropagationOption handleNullPropagation = HandleNullPropagationOption.Default)
-            where TModel : class
-            => await query.GetAsync(mapper, options, new QuerySettings { ODataSettings = new ODataSettings { HandleNullPropagation = handleNullPropagation } });
-
-        /// <summary>
-        /// GetQueryAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="options"></param>
-        /// <param name="handleNullPropagation"></param>
-        /// <returns></returns>
-        [Obsolete("\"Use Task<IQueryable<TModel>> GetQueryAsync<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings = null)\"")]
-        public static async Task<IQueryable<TModel>> GetQueryAsync<TModel, TData>(this IQueryable<TData> query, IMapper mapper, ODataQueryOptions<TModel> options, HandleNullPropagationOption handleNullPropagation = HandleNullPropagationOption.Default)
-            where TModel : class
-            => await query.GetQueryAsync(mapper, options, new QuerySettings { ODataSettings = new ODataSettings { HandleNullPropagation = handleNullPropagation } });
 
         /// <summary>
         /// GetQueryAsync
@@ -193,106 +148,6 @@ namespace AutoMapper.AspNet.OData
                         asyncSettings?.CancellationToken ?? default
                     )
             ).ToList();
-
-        /// <summary>
-        /// GetQueryAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="filter"></param>
-        /// <param name="queryFunc"></param>
-        /// <param name="includeProperties"></param>
-        /// <param name="projectionSettings"></param>
-        /// <returns></returns>
-        [Obsolete]
-        public static async Task<IQueryable<TModel>> GetQueryAsync<TModel, TData>(this IQueryable<TData> query, IMapper mapper,
-            Expression<Func<TModel, bool>> filter = null,
-            Expression<Func<IQueryable<TModel>, IQueryable<TModel>>> queryFunc = null,
-            IEnumerable<Expression<Func<TModel, object>>> includeProperties = null,
-            ProjectionSettings projectionSettings = null)
-        {
-            //Map the expressions
-            Expression<Func<TData, bool>> f = mapper.MapExpression<Expression<Func<TData, bool>>>(filter);
-            Func<IQueryable<TData>, IQueryable<TData>> mappedQueryFunc = mapper.MapExpression<Expression<Func<IQueryable<TData>, IQueryable<TData>>>>(queryFunc)?.Compile();
-
-            if (filter != null)
-                query = query.Where(f);
-
-            return await Task.Run
-            (
-                () => mappedQueryFunc != null
-                    ? mapper.ProjectTo(mappedQueryFunc(query), projectionSettings?.Parameters, GetIncludes())
-                    : mapper.ProjectTo(query, projectionSettings?.Parameters, GetIncludes())
-            );
-
-            Expression<Func<TModel, object>>[] GetIncludes() => includeProperties?.ToArray() ?? new Expression<Func<TModel, object>>[] { };
-        }
-
-        /// <summary>
-        /// QueryAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <typeparam name="TModelReturn"></typeparam>
-        /// <typeparam name="TDataReturn"></typeparam>
-        /// <typeparam name="TReturn"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="queryFunc"></param>
-        /// <param name="includeProperties"></param>
-        /// <returns></returns>
-        [Obsolete]
-        public static async Task<TReturn> QueryAsync<TModel, TData, TModelReturn, TDataReturn, TReturn>(this IQueryable<TData> query, IMapper mapper,
-            Expression<Func<IQueryable<TModel>, TModelReturn>> queryFunc,
-            IEnumerable<Expression<Func<TModel, object>>> includeProperties = null)
-        {
-            Func<IQueryable<TData>, TDataReturn> mappedQueryFunc = mapper.MapExpression<Expression<Func<IQueryable<TData>, TDataReturn>>>(queryFunc).Compile();
-            ICollection<Expression<Func<TData, object>>> includes = mapper.MapIncludesList<Expression<Func<TData, object>>>(includeProperties);
-
-            if (includes != null)
-                query = includes.Aggregate(query, (q, next) => q.Include(next));
-
-            TDataReturn result = await Task.Run(() => mappedQueryFunc(query));
-
-            return typeof(TReturn) == typeof(TDataReturn) ? (TReturn)(object)result : mapper.Map<TDataReturn, TReturn>(result);
-        }
-
-        /// <summary>
-        /// QueryAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <typeparam name="TModelReturn"></typeparam>
-        /// <typeparam name="TDataReturn"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="queryFunc"></param>
-        /// <param name="includeProperties"></param>
-        /// <returns></returns>
-        [Obsolete]
-        public static async Task<TModelReturn> QueryAsync<TModel, TData, TModelReturn, TDataReturn>(this IQueryable<TData> query, IMapper mapper,
-            Expression<Func<IQueryable<TModel>, TModelReturn>> queryFunc,
-            IEnumerable<Expression<Func<TModel, object>>> includeProperties = null)
-            => await query.QueryAsync<TModel, TData, TModelReturn, TDataReturn, TModelReturn>(mapper, queryFunc, includeProperties);
-
-        /// <summary>
-        /// QueryAsync
-        /// </summary>
-        /// <typeparam name="TModel"></typeparam>
-        /// <typeparam name="TData"></typeparam>
-        /// <typeparam name="TModelReturn"></typeparam>
-        /// <param name="query"></param>
-        /// <param name="mapper"></param>
-        /// <param name="queryFunc"></param>
-        /// <param name="includeProperties"></param>
-        /// <returns></returns>
-        [Obsolete]
-        public static async Task<TModelReturn> QueryAsync<TModel, TData, TModelReturn>(this IQueryable<TData> query, IMapper mapper,
-            Expression<Func<IQueryable<TModel>, TModelReturn>> queryFunc,
-            IEnumerable<Expression<Func<TModel, object>>> includeProperties = null)
-            => await query.QueryAsync<TModel, TData, TModelReturn, TModelReturn, TModelReturn>(mapper, queryFunc, includeProperties);
 
         private static IQueryable<TData> GetDataQuery<TModel, TData>(this IQueryable<TData> query, IMapper mapper,
             Expression<Func<TModel, bool>> filter = null,
