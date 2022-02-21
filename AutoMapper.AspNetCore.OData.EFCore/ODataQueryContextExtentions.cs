6e806372a1911edc11fb16e83717466a81dc35bc
Warren <wbuckley15@hotmail.com>
2022-02-20 21:06:03 -0500

    Use model to find entity

--- AutoMapper.AspNetCore.OData.EFCore/ODataQueryContextExtentions.cs
+++ AutoMapper.AspNetCore.OData.EFCore/ODataQueryContextExtentions.cs
@@ -11,41 +11,9 @@ namespace AutoMapper.AspNet.OData
         {
             context = context ?? throw new ArgumentNullException(nameof(context));
 
-            if (context.ElementType is IEdmEntityType parent)
-            {
-                if (parent.Name.Equals(type.Name, StringComparison.Ordinal))
-                    return FindProperties(parent);
+            var entity = context.Model.FindDeclaredType(type.FullName) as IEdmEntityType;
+            return entity is not null ? FindProperties(entity) : null;
 
-                var child = FindEntity(type, parent);
-                if (child is not null)
-                    return FindProperties(child);
-            }
-            return null;
-
-            static IEdmEntityType FindEntity(Type type, IEdmEntityType declaringType)
-            {
-                var props = declaringType.DeclaredProperties
-                    .Where(p => p.Type.Definition is IEdmCollectionType)
-                    .Select(p => (IEdmCollectionType)p.Type.Definition)
-                    .Where(p => p.ElementType.Definition is IEdmEntityType)
-                    .Select(p => (IEdmEntityType)p.ElementType.Definition)
-                    .Distinct();
-
-                if (props.Any())
-                {
-                    var found = props.FirstOrDefault(p =>
-                        p.Name.Equals(type.Name, StringComparison.Ordinal));
-
-                    if (found is not null)
-                        return found;
-
-                    foreach (var prop in props)
-                    {
-                        return FindEntity(type, prop);
-                    }
-                }
-                return null;
-            }
 
             static OrderBySetting FindProperties(IEdmEntityType entity)
             {
