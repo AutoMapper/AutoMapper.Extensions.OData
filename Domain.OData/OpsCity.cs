5d2fa013d40466869cc70faede5fd8a65f2b46e6
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-04-15 10:34:43 -0400

    Fixes Issue #132 : Inherited DTO properties appear to be ignored.

--- Domain.OData/OpsCity.cs
+++ Domain.OData/OpsCity.cs
@@ -4,9 +4,13 @@ using System.Text;
 
 namespace Domain.OData
 {
-    public class OpsCity
+    public class OpsCity : BaseOpsCity
     {
         public Int32 Id { get; set; }
+    }
+
+    public abstract class BaseOpsCity
+    {
         public String Name { get; set; }
     }
 }
